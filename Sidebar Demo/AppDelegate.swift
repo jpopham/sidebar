3715447b4e229d0c2420c5271c978fe0c7343e48
jpopham <john_popham@hotmail.com>
2015-01-23 16:12:56 +0000

    Initial Commit

---
+++ Sidebar Demo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Sidebar Demo
+//
+//  Created by John Popham on 23/01/2015.
+//  Copyright (c) 2015 xxx. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
